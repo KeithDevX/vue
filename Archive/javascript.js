b33f30604c7ff839508911c015302ec0e2926f4c
walsk027 <keith.d.walsh@disney.com>
2020-04-18 15:26:03 -0700

    Training Directory Sync

---
+++ Archive/javascript.js
@@ -0,0 +1,12 @@
+
+
+var divs = document.getElementsByTagName('div');
+
+for (let i = 0; i < divs.length;i++){
+
+
+    divs[i].addEventListener('click',function(){
+        console.log('you clocked div #' + i)
+
+    })
+}
